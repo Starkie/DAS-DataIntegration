2f0fce6824d4dc3bef5de7edd3c42e308512f7b5
Adriano Vega <adrianovega@hotmail.com>
2020-01-26 22:09:55 +0100

    Created the initial migration

---
+++ DataLoader/Migrations/20200126210701_InitialState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLoader.Migrations
+{
+    public partial class InitialState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Gender = table.Column<int>(nullable: false),
+                    Age = table.Column<int>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    RegistrationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
