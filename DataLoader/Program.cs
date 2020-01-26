c50b5cd44f2ec9f6c3c4d8a85cd73eea593ff494
Adriano Vega <adrianovega@hotmail.com>
2020-01-27 00:50:33 +0100

    Created the action for the LoadUserArtistsPlaysToDatabase

--- DataLoader/Program.cs
+++ DataLoader/Program.cs
@@ -3,10 +3,12 @@
     using System;
     using System.Globalization;
     using System.IO;
+    using System.Linq;
     using CsvHelper;
     using DataLoader.Entities;
     using DataLoader.Persistence;
     using McMaster.Extensions.CommandLineUtils;
+    using Microsoft.EntityFrameworkCore;
 
     class Program
     {
@@ -16,16 +18,18 @@
             app.HelpOption();
 
             CommandArgument usersFileOption = app.Argument("-u|--usersFile", "The path to the CSV file containing the user data.");
+            CommandArgument userArtistPlaysFileOption = app.Argument("-p|--userArtistPlaysFile", "The path to the CSV file containing the user - artist plays data.");
 
             app.OnExecute(() =>
             {
-                LoadCsvToDatabase(usersFileOption.Value);
+                LoadUsersToDatabase(usersFileOption.Value);
+                LoadUserArtistsPlaysToDatabase(userArtistPlaysFileOption.Value);
             });
 
             return app.Execute(args);
         }
 
-        private static void LoadCsvToDatabase(string usersFilePath)
+        private static void LoadUsersToDatabase(string usersFilePath)
         {
             using StreamReader streamReader = new StreamReader(usersFilePath);
             using CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
@@ -56,6 +60,61 @@
                 Console.WriteLine($"READ: {user.Id} - Gender: {user.Gender} Age: {user.Age} Country: {user.Country} RegistrationDate: {user.RegistrationDate}");
 
                 context.Users.Add(user);
+            }
+
+            context.SaveChanges();
+        }
+
+        private static void LoadUserArtistsPlaysToDatabase(string userArtistPlaysFilePath)
+        {
+            using StreamReader streamReader = new StreamReader(userArtistPlaysFilePath);
+            using CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            using DataLoaderContext context = new DataLoaderContext();
+            context.Database.EnsureCreated();
+
+            csvReader.Configuration.HasHeaderRecord = true;
+            csvReader.Configuration.AllowComments = true;
+            csvReader.Configuration.Delimiter = "\t";
+
+            csvReader.Configuration.BadDataFound = (context) =>
+            {
+                Console.WriteLine($"BAD RECORD - {context.RawRecord}");
+            };
+
+            while (csvReader.Read())
+            {
+                string userId = csvReader.GetField(0);
+                string artistId = csvReader.GetField(1);
+                string artistName = csvReader.GetField(2);
+                long.TryParse(csvReader.GetField(3), out long userArtistPlaysNumber);
+
+                User user = context.Users.FirstOrDefault(u => u.Id == userId);
+                Artist artist = context.Artists.FirstOrDefault(a => a.Id == artistId);
+
+                if (artist == null)
+                {
+                    artist = new Artist
+                    {
+                    Id = artistId,
+                    Name = artistName,
+                    };
+
+                    context.Artists.Add(artist);
+
+                    // Save changes to avoid conflict with change tracker values.
+                    context.SaveChanges();
+                }
+
+                UserArtistPlays userArtistPlays = new UserArtistPlays
+                {
+                    Artist = artist,
+                    User = user,
+                    PlaysNumber = userArtistPlaysNumber,
+                };
+
+                Console.WriteLine($"READ: {userArtistPlays.User.Id} - ArtistName: {userArtistPlays.Artist.Name} Plays: {userArtistPlays.PlaysNumber}");
+
+                context.UserArtistPlays.Add(userArtistPlays);
             }
 
             context.SaveChanges();
