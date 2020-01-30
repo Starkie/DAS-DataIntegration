ab4c0dba56c58c932bfbb31a089cab26ea509fe1
Adriano Vega <adrianovega@hotmail.com>
2020-01-30 21:23:02 +0100

    Created database migration to add the UserPlaysCsv table

---
+++ DataLoader/Migrations/20200127233201_ArtistsAndUserPlaysCsv.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLoader.Migrations
+{
+    public partial class ArtistsAndUserPlaysCsv : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserPlaysCsvs",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<string>(nullable: true),
+                    ArtistId = table.Column<string>(nullable: true),
+                    ArtistName = table.Column<string>(nullable: true),
+                    Plays = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPlaysCsvs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPlaysCsvs");
+        }
+    }
+}
