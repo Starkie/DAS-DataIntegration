dcd8525ed5fe27766a20b063d42c2fa23eb7609f
Adriano Vega <adrianovega@hotmail.com>
2020-01-31 22:01:04 +0100

    Created a migration to add the Genre column to Artist

---
+++ DataLoader/Migrations/20200131210018_AddGenreToArtist.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLoader.Migrations
+{
+    public partial class AddGenreToArtist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Artists",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Artists");
+        }
+    }
+}
